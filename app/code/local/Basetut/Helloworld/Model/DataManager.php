48ec019ef9f7c2e4802ea0321ac8539606ae85fc
tuandvsmartosc <tuandv@smartosc.com>
2017-10-16 14:54:53 +0700

    commit ee

--- app/code/local/Basetut/Helloworld/Model/DataManager.php
+++ app/code/local/Basetut/Helloworld/Model/DataManager.php
@@ -6,10 +6,27 @@ class Basetut_Helloworld_Model_DataManager extends Mage_Catalog_Model_Abstract{
             //get getcollection. ham getcolloction de tao cau query
             ->addAttributeToSelect('*')
             ->setOrder('entity_id', 'ASC')
-            ->setPageSize(100);
+            ->setPageSize(100)
+        //get default bill setting
+        ->joinAttribute('billing_street', 'customer_address/street', 'default_billing', null, 'left')
+            ->joinAttribute('billing_postcode', 'customer_address/postcode', 'default_billing', null, 'left')
+            ->joinAttribute('billing_city', 'customer_address/city', 'default_billing', null, 'left')
+            ->joinAttribute('billing_telephone', 'customer_address/telephone', 'default_billing', null, 'left')
+            ->joinAttribute('billing_fax', 'customer_address/fax', 'default_billing', null, 'left')
+            ->joinAttribute('billing_region', 'customer_address/region', 'default_billing', null, 'left')
+            ->joinAttribute('billing_country_code', 'customer_address/country_id', 'default_billing', null, 'left')
+            //get default ship setting
+            ->joinAttribute('shipping_street', 'customer_address/street', 'default_shipping', null, 'left')
+            ->joinAttribute('shipping_postcode', 'customer_address/postcode', 'default_shipping', null, 'left')
+            ->joinAttribute('shipping_city', 'customer_address/city', 'default_shipping', null, 'left')
+            ->joinAttribute('shipping_telephone', 'customer_address/telephone', 'default_shipping', null, 'left')
+            ->joinAttribute('shipping_fax', 'customer_address/fax', 'default_shipping', null, 'left')
+            ->joinAttribute('shipping_region', 'customer_address/region', 'default_shipping', null, 'left')
+            ->joinAttribute('shipping_country_code', 'customer_address/country_id', 'default_shipping', null, 'left')
+            ->joinAttribute('taxvat', 'customer/taxvat', 'entity_id', null, 'left');
         foreach ($customer as $custome) {
 //            var_dump($custome->getData());die;
-            $arr_customer[] = $custome->toArray(array('entity_id', 'firstname' ,'lastname', 'email', 'group_id'));
+            $arr_customer[] = $custome->toArray(array('entity_id', 'firstname' ,'lastname', 'email', 'group_id', 'shipping_street','billing_city'));
         }
 
         return $arr_customer;
